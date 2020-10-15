8a1ed612c133373123280f02b99b76787580f472
Your Name <prateekpriyesh@gmail.com>
2020-10-16 01:21:04 +0530

    Updates

---
+++ precache-manifest.6560f5bfcafb2fbaf169a9bf96bc32d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad47b16a254c7f47ca5d60ee574e3f0",
+    "url": "/UserData/index.html"
+  },
+  {
+    "revision": "449e3c2298feb33eb647",
+    "url": "/UserData/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5be9713551c1da60b6e3",
+    "url": "/UserData/static/js/2.694ab930.chunk.js"
+  },
+  {
+    "revision": "fd77f13855b5676a8e117c5b6583996c",
+    "url": "/UserData/static/js/2.694ab930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50be8e2455983c1e8a18",
+    "url": "/UserData/static/js/3.a9c349f1.chunk.js"
+  },
+  {
+    "revision": "da5e3c79de5c0bd1c369",
+    "url": "/UserData/static/js/4.106e8f20.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/UserData/static/js/4.106e8f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8939b5e7607f98a8bd3",
+    "url": "/UserData/static/js/5.69d5014f.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/UserData/static/js/5.69d5014f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449e3c2298feb33eb647",
+    "url": "/UserData/static/js/main.093ed7e6.chunk.js"
+  },
+  {
+    "revision": "cc1e81efdb833533355a",
+    "url": "/UserData/static/js/runtime-main.65d12e85.js"
+  }
+]);
